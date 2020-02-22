2446dd065d51eff2e86a1ea1713d790f2083d189
KBHELLO <35730869+KBHELLO@users.noreply.github.com>
2020-02-23 02:51:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ cameraOptions.onchange = () => {
       exact: cameraOptions.value
     }
   };
-  alert(updatedConstraints);
   startStream(updatedConstraints);
 };
 
@@ -88,7 +87,7 @@ const handleStream = (stream) => {
 
 const getCameraSelection = async () => {
   const devices = await navigator.mediaDevices.enumerateDevices();
-  console.log('devices ', devices)
+      alert('devices ',devices);
   const videoDevices = devices.filter(device => device.kind === 'videoinput');
   const options = videoDevices.map(videoDevice => {
     return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
